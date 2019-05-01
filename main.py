8a6e2428b4146186b8d5dc9e938f49a9d9b170af
Akash Manna <akashmanna@gmail.com>
2019-05-01 23:36:17 +0530

    code cleanup

--- main.py
+++ main.py
@@ -7,12 +7,8 @@ from vlc import VLC
 from darknet.darknet import detect
 from gui import GUI
 
-FILENAME = "WatchtowerOfTurkey.MKV"
-#FILENAME = "TownCentreXVID.avi"
-#FILENAME = 0
 
-
-
-mainApp = GUI()
+if __name__ == "__main__":
+    mainApp = GUI()
 
 
